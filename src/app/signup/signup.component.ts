204fa2da45fad7c775df627b037d893200dcc63e
zerry101 <macone394@gmail.com>
2023-01-18 22:35:16 +0530

    m

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -8,12 +9,35 @@ import { FormControl } from '@angular/forms';
 })
 export class SignupComponent {
 
-  username:any=new FormControl("");
+  name:any=new FormControl("");
+// username=new FormControl("");
+  // userForm:FormGroup | undefined;
+
+
+  clear_name(){
+    // 1 ->this.username.reset();
+    //2  below
+    this.name.setValue("");
+  }
+
+  userForm:FormGroup=new FormGroup({
+    username:new FormControl(""),
+    password:new FormControl(""),
+    confirmpassword:new FormControl("")
+  });
+
+
+  submit()
+  {
+    console.log(this.userForm.value);
+
+  }
 
   clear(){
     // 1 ->this.username.reset();
     //2  below
-    this.username.setValue("");
+    this.userForm.reset();
   }
+
 
 }
