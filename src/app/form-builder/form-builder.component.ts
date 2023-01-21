01789af54a196ba1be67152ade4a15658c2b7aba
zerry101 <macone394@gmail.com>
2023-01-21 22:45:48 +0530

    m

--- src/app/form-builder/form-builder.component.ts
+++ src/app/form-builder/form-builder.component.ts
@@ -1,5 +1,33 @@
 import { Component } from '@angular/core';
 
+interface Ia{
+  name:string|undefined,
+  roll:number|undefined
+}
+
+interface Ib{
+  marks:Array<number>|undefined,
+  stu_names:Array<string>|undefined
+}
+
+interface ic extends Ia,Ib{
+
+}
+
+
+class student1 implements ic
+{
+constructor(){
+  this.marks=[20,20,20];
+  ui:String;
+}
+  name: string | undefined;
+  roll: number | undefined;
+  marks: number[] | undefined;
+  stu_names: string[] | undefined;
+
+}
+
 @Component({
   selector: 'app-form-builder',
   templateUrl: './form-builder.component.html',
@@ -8,5 +36,15 @@ import { Component } from '@angular/core';
 export class FormBuilderComponent {
 
   constructor(){}
+
+  stu1 =  new student1();
+
+    print(){
+      console.log(this.stu1.marks);
+    }
+
+
+
+
 
 }
