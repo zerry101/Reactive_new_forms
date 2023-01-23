cb01e55ec26ea080cc459d070e9d83e7d49e2d29
zerry101 <macone394@gmail.com>
2023-01-23 21:53:45 +0530

    m

--- src/app/observer/observer.component.ts
+++ src/app/observer/observer.component.ts
@@ -10,25 +10,30 @@ export class ObserverComponent implements OnInit {
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
     // this.myObservable.subscribe((val)=>{
-    //   console.log(val);
+      //   console.log(val);
+
+     this.Observable =this.myObservable;
+
+
 
     // })
   }
 
   myObservable:Observable<any> = new Observable((Observer) => {
     console.log("Observable starts");
-    // setTimeout(()=>{Observer.next("1");},1000);
-    // setTimeout(()=>{Observer.next("2");},2000);
-    // setTimeout(()=>{Observer.next("3");},3000);
-    // setTimeout(()=>{Observer.next("4");},4000);
-    // setTimeout(()=>{Observer.next("5");},5000);
+    setTimeout(()=>{Observer.next("1");},1000);
+    setTimeout(()=>{Observer.next("2");},2000);
+    setTimeout(()=>{Observer.next("3");},3000);
+    setTimeout(()=>{Observer.next("4");},4000);
+    setTimeout(()=>{Observer.next("5");},5000);
 
-    Observer.next("2");
-    Observer.next("3");
-    Observer.next("4");
-    Observer.next("5");
+    // Observer.next("2");
+    // // Observer.next("3");
+    // Observer.next("4");
+    // Observer.next("5");
   })
 
+  Observable:Observable<any> | undefined;
 
 
 
